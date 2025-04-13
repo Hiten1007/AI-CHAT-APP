2619546b57730308d9a0d5d41ae8befa28db663b
keerthana <149101981+keerthanathummala@users.noreply.github.com>
2025-04-13 18:39:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './hooks/useAuth';
+import Home from './pages/Home';
+ import Auth from './pages/Auth';
+import ChatPage from './pages/ChatPage';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/chat" /> : <Home authUser={user} />} />
+         <Route path="/auth" element={user ? <Navigate to="/chat" /> : <Auth />} />
+        <Route path="/chat"s element={user ? <ChatPage /> : <Navigate to="/" />} /> 
+      </Routes>
+      <Toaster position="top-right" />
+    </Router>
+  );
+}
+
+export default App;
